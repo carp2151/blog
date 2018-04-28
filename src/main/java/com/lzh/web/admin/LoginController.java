eb4e8bd3d86b410de044e7d1b612a4e60e1615d9
BYHZ\lizhenghua <wb-lzh273493@alibaba-inc.com>
2018-04-28 13:04:33 +0800

    提交博客全部功能

---
+++ src/main/java/com/lzh/web/admin/LoginController.java
@@ -0,0 +1,58 @@
+package com.lzh.web.admin;
+
+import com.lzh.po.User;
+import com.lzh.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by limi on 2017/10/15.
+ */
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String loginPage() {
+        return "admin/login";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "admin/index";
+    }
+
+
+    @PostMapping("/login")
+    public String login(@RequestParam String username,
+                        @RequestParam String password,
+                        HttpSession session,
+                        RedirectAttributes attributes) {
+        User user = userService.checkUser(username, password);
+        if (user != null) {
+            user.setPassword(null);
+            session.setAttribute("user",user);
+            return "redirect:/admin/index";
+        } else {
+            attributes.addFlashAttribute("message", "用户名和密码错误");
+            return "redirect:/admin";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("user");
+        return "redirect:/admin";
+    }
+}
